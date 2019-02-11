81e5c84c2d6c7bf26e2cb9b29334665f24178fc4
Dario D'Amico <DD'Amico@esri.com>
2019-02-11 07:54:09 -0800

    Same POV in incubator

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,8 @@ const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const gl = canvas.getContext("webgl");
 const app = new Application(vec4.fromValues(0.2, 0.3, 0.5, 1.0));
 
-app.setView([0, 0], 0, 1);
-
 function render() {
+  app.setView([-10539183.811482586 + 1000 * Math.cos(performance.now() / 2000), 4651153.046248602 + 1000 * Math.sin(performance.now() / 1000)], 0, 4.777);
   app.render(gl);
   requestAnimationFrame(render);
 }
